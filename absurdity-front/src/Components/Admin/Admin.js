e585ca7a0cf70eb9d0a47ecefc3339a1840c18e3
Nabiha Quebaili <nabiha.quebaili@live.fr>
2022-09-29 15:49:17 +0200

    [FUNC] Admin page input + question item

--- absurdity-front/src/Components/Admin/Admin.js
+++ absurdity-front/src/Components/Admin/Admin.js
@@ -1,6 +1,23 @@
+import React from 'react';
+import { Input, Segment, Icon, Button } from 'semantic-ui-react';
+
+
 function Admin() {
     return ( 
-        
+       <>
+        <h1>Créez votre question</h1>
+        <Input action='Valider' placeholder='Écrivez votre question' />
+
+        <Segment>Quelle est la moitié de tout?
+      <Button icon>
+        <Icon name='pencil alternate' labelPosition='left' />
+      </Button>
+      <Button icon>
+        <Icon name='trash alternate' labelPosition='right' />
+      </Button>
+        </Segment>
+    
+      </>
      );
 }
 
