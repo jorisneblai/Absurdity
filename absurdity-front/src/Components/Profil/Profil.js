d20263601297f8e1e7b4f4baa3b31e773f834e59
Joris Neblai <joris.neblai@icloud.com>
2022-09-29 15:03:08 +0200

    [EDIT][Profil] added content of the profil page, with title and inputs

--- absurdity-front/src/Components/Profil/Profil.js
+++ absurdity-front/src/Components/Profil/Profil.js
@@ -1,6 +1,25 @@
+import { Input, Label } from 'semantic-ui-react';
+import './Profil.scss';
+
 function Profil() {
     return (  
-        
+        <main className="Profil">
+            <h1 className="Profil-title">
+                Profil de
+            </h1>
+            <form className="Profil-inputs">
+                <Input className="Pseudo-input" labelPosition='right' type='text' placeholder='Changer de pseudo...'>
+                    <Label className='Pseudo-label'>Pseudo</Label>
+                    <input />
+                    <Label icon='pencil alternate'></Label>
+                </Input>
+                <Input labelPosition='right' type='password' placeholder='Changer de mot de passe...'>
+                    <Label className='Pwd-label'>Mot de passe</Label>
+                    <input />
+                    <Label icon='pencil alternate'></Label>
+                </Input>
+            </form>
+        </main>
     );
 }
 
